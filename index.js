b6cdf600e2f8f036d8ecce9046c82cb8ee0cfeca
Mely07 <damely.tineo07@gmail.com>
2020-07-10 19:19:45 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const answer = tutorials.map((tutorial) => {
+    let x = tutorial.split(' ').map( w =>  w.substring(0,1).toUpperCase()+ w.substring(1)).join(' ');
+    return x;
+  });
+  return answer ;
 }
