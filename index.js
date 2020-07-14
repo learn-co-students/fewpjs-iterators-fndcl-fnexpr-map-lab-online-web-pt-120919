f01da9b490279aa3d6de3888ff0a5001f721f0d7
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-07-14 13:18:42 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentence => {
+    const words = sentence.split(" ") 
+    const capitalized = words.map(word => {
+     return word.charAt(0).toUpperCase() + word.slice(1) 
+    })
+      return capitalized.join(' ') 
+  } ) 
+  
+} 
+
